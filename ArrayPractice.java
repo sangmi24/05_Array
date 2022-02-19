046818c35c53b0355c94fe08082fff8f0b118a52
sangmi <97028831+sangmi24@users.noreply.github.com>
2022-02-19 23:41:40 +0900

    add new files

--- ArrayPractice.java
+++ ArrayPractice.java
@@ -8,16 +8,17 @@ public class ArrayPractice {
 		//길이가 10인 정수형 배열을 만들고 반복문 이용
 		//역순으로 배열 인덱스에 넣은 후 그 값을 출력하세요.
 		int[] arr = new int[10];	
-		int num=10;		
+	    int num=10;		
 		//배열 만들기
-		for(int i=0; i<arr.length; i++) {
+		for(int i=0; i<10; i++) {
 			arr[i]=num;
-			num--;
-		}
+			num--;		}
 		//출력
-		for(int i=0; i<arr.length; i++ ) {
+		for(int i=0; i<10; i++ ) {
 			System.out.print(arr[i]+ " ");
 		}
+		
+
         
 	}
 	public void practice9() {
@@ -45,5 +46,110 @@ public class ArrayPractice {
        }
 	}
   }
+        public  void practice1() {
+        	
+        	int[] num = new int[10];
+        	for (int i=1; i<11; i++) {
+        		System.out.print(i+" ");
+        	}  	
+        }
+      
+        public void practice3() {
+        	//사용자에게 입력 받은 양의 정수만큼 배열 크기를 할당하고
+        	//1부터 입력 받은 값까지 배열에 초기화한후 출력하세요.
+        	Scanner sc = new Scanner(System.in);
+        	System.out.print("양의 정수 : ");
+        	int num = sc.nextInt();
+        	
+        	int[] arr = new int[num];  
+        	for(int i=1; i<= (arr.length); i++) {
+        		System.out.print(i+" "); 	
+        	}
+        }
+	   
+        public void practice4() {
+        	//길이가 5인 String배열을 선언, 과일이름으로 초기화한후
+        	//배열 인덱스를 활용해서 귤을 출력하세요.
+        	String[] fruits ={"사과","귤","포도","복숭아","참외"};
+        	
+        	System.out.println( fruits[1]);
 
-}
+        }
+	     
+        public void practice5() {
+        	//문자열을 입력 받아 문자 하나하나를 배열에 넣고 
+        	//검색할 문자가 문자열에 몇개 들어가 있는지 개수와 몇번째 인덱스에 위치하는지 인덱스를 출력하세요.
+        	Scanner sc = new Scanner(System.in);
+        	
+        	System.out.print("문자열: ");
+            String str1 =sc.nextLine();
+            
+            System.out.print("문자: ");
+            char ch =sc.next().charAt(0);
+            
+            char[] arr = new char[str1.length()];
+            String index="";  //인덱스초기화
+            int count =0;     //정수 초기화
+            
+            for(int i=0; i<arr.length; i++) {
+            	arr[i] = str1.charAt(i);  //문자열중에 문자뽑는거
+            	if(ch == arr[i]) {
+            		index = index + i +" ";  //index += i
+            		count++;
+            	}
+            }
+        	System.out.println(str1+"에 "+ch+"가 존재하는 위치(인덱스) : "+ index );
+        	System.out.println(ch+" 개수 : "+count );
+        }
+       
+           public void practice6() {
+        	   //"월"~ "일"(요일)까지 초기화된 문자열 배열을 만들고 0부터 6까지 숫자를 입력 받아
+        	   //입력한 숫자와 같은 인덱스에 있는 요일을 출력하고
+        	   //범위에 없는 숫자를 입력시 "잘못 입력하셨습니다"를 출력하세요.
+        	   
+        	   Scanner sc = new Scanner(System.in);
+        	   System.out.print("0~부터 6 사이 숫자 입력 : ");
+        	   int num = sc.nextInt();
+        	   
+        	   
+        	   String[] day = {"월요일","화요일","수요일","목요일","금요일","토요일","일요일"} ;
+        	   
+                        	   
+        	   
+        	   
+           }}
+        	   
+           
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+	
+	
+
